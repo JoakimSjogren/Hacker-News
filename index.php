036186d67677873287fa423a03623f5560a6d695
JoakimSjogren <70525815+JoakimSjogren@users.noreply.github.com>
2020-12-18 06:30:08 +0100

    Basic structure

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
 <?php
-    
+    require __DIR__ . "/views/header.php";
+    require __DIR__ . "/views/navigation.php";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
     
 </body>
 </html>
---
+++ views/navigation.php
@@ -0,0 +1,8 @@
+<nav>
+    <ul>
+        <li>aa</li>
+        <li>aa</li>
+        <li>aa</li>
+        <li><a href = "">Login</a></li>
+    </ul>
+</nav>
