511a5b0bcde0709d47f7597ce782eea382778567
JoakimSjogren <70525815+JoakimSjogren@users.noreply.github.com>
2020-12-20 06:59:10 +0100

    Login

--- app/user/login.php
+++ app/user/login.php
@@ -1,12 +1,18 @@
 <?php
-    require __DIR__ . ('../function.php');
-
+    // require __DIR__ . ('./function.php');
+    
     if (isset($_POST['email'], $_POST['password'])) {
         $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
         $password = $_POST['password'];
 
-        $pdo = new PDO('sqlite:hacker.db');
-        // $statement = $pdo->prepare('SELECT *');
-        
+        $pdo = new PDO('sqlite:hacker.sqlite');
+
+        $statement = $pdo->prepare('SELECT * from Users WHERE email = :email');
+        $statement->bindparam(':email', $email, PDO::PARAM_STR);
+        $statement->execute();
+
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
+       
+        die(var_dump($user));
     }
 ?>
